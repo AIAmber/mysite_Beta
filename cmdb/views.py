b5dba7e01576985e698806127e270bc17d5f797f
aiamber <lzlrjok@vip.qq.com>
2017-07-09 18:58:12 +0800

    the initial mysite files.

---
+++ cmdb/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+# Create your views here.
+
+def index(request):
+    # request.POST
+    # request.GET
+    #return HttpResponse("Hello, world!")
+    return  render(request, 'tangshi.html')
